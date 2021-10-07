eaa9408a716f37bae0dea21c8e3604944ee2109d
Felipe Tadeu <felipe.tadeusantos1@gmail.com>
2021-10-07 11:33:17 -0300

    restaura dado

--- api/controllers/NivelController.js
+++ api/controllers/NivelController.js
@@ -62,7 +62,17 @@ class NivelController{
             await database.Niveis.destroy( { where: { id: Number(id) }})
             return res.status(200).json({ mensagem: `id ${id} deletado`})
         } catch (error) {
-            return res.status(200).json(error.message)
+            return res.status(500).json(error.message)
+        }
+    }
+
+    static async restauraNivel(req, res){
+        const { id } = req.params
+        try {
+            await database.Niveis.restore({ where: { id: Number(id) }})
+            return res.status(200).json({ mensagem: `id ${id} restaurado`})
+        } catch (error) {
+            return res.status(500).json(error.message)
         }
     }
 }
