6f1636f79ee170f7b97c212e27fb262874b9c8d5
Felipe Tadeu <felipe.tadeusantos1@gmail.com>
2021-10-08 17:09:06 -0300

    transaction

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -219,6 +219,21 @@ class PessoaController {
         }
     }
 
+    static async cancelaPessoa(req, res) {
+        const { estudanteId } = req.params
+        try {
+            database.Sequelize.transaction(async transacao => {
+                await database.Pessoas.update({ ativo: false }, {where: {id: Number(estudanteId)}}, {transaction: transacao})
+                await database.Matriculas.update({ status: 'cancelado'}, {where: {estudante_id: Number(estudanteId)}}, {transaction: transacao})
+    
+                return res.status(200).json({message: `Matrículas ref. estudante ${estudanteId} canceladas`})
+
+            })
+        } catch (error) {
+            return res.status(500).json(error.message)
+        }
+    }
+
 }
 
 module.exports = PessoaController
